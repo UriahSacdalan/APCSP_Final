1f1891a8f51ac9029b52a2dac9c7f27ae2d76e8b
Uriah <uriahnaomi.sacdalan@vtsd.com>
2023-01-06 12:29:18 -0500

    s

---
+++ Hangman.java
@@ -0,0 +1,8 @@
+public class Hangman {
+    public static String Input()
+    {
+        System.out.println("")
+
+    }
+    System.out.println("")
+}
---
+++ MainHangman.java
@@ -0,0 +1,7 @@
+public class MainHangman
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
