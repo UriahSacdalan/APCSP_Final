97402968f9bd3a9e42d45c9e7b6f98dcde9c2519
Uriah <uriahnaomi.sacdalan@vtsd.com>
2023-01-18 13:11:47 -0500

    dnf

--- Hangman.java
+++
@@ -1,8 +0,0 @@
-public class Hangman {
-    public static String Input()
-    {
-        System.out.println("")
-
-    }
-    System.out.println("")
-}
--- MainHangman.java
+++ MainHangman.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
+/* Needs: selection (if), iteration (loop i.e. for), sequences (put it in order), user-defined methods that call other user-defined methods
+*/
 public class MainHangman
 {
     public static void main(String[] args)
     {
+        Scanner test = new Scanner(System.in);
+        System.out.println("Enter letter");
+
+        String letter = test.nextLine(); //read user input
+        if(letter == )
+            System.out.println("Username: " + letter); //Output user input
 
     }
 }
