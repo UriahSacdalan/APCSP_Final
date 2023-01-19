42c220231f23db876323e3ae73a48cfd9415d668
Uriah <uriahnaomi.sacdalan@vtsd.com>
2023-01-19 12:24:32 -0500

    dnf

--- MainHangman.java
+++ MainHangman.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.Random;
 /* Needs: selection (if), iteration (loop i.e. for), sequences (put it in order), user-defined methods that call other user-defined methods
 */
 public class MainHangman
@@ -7,11 +7,14 @@ public class MainHangman
     public static void main(String[] args)
     {
         Scanner test = new Scanner(System.in);
-        System.out.println("Enter letter");
+        System.out.print("Enter letter: ");
 
         String letter = test.nextLine(); //read user input
-        if(letter == )
-            System.out.println("Username: " + letter); //Output user input
-
+        if(letter .equals(list))
+            System.out.println("Letter: " + letter); //Output user input
+        else{
+            System.out.println("That is not a valid letter.");
+        }
     }
+
 }
