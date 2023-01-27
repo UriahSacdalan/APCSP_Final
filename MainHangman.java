65d52fa5b08b6d66766c3809741b6e3cd85e5688
Uriah <uriahnaomi.sacdalan@vtsd.com>
2023-01-27 13:54:26 -0500

    dnf

--- MainHangman.java
+++ MainHangman.java
@@ -4,7 +4,7 @@ import java.util.Random;
 */
 public class MainHangman
 {
-    public static void main(String[] args)
+    /*public static void main(String[] args)
     {
         Random testRand = new Random();
         String wordList[] = {"art", "scone", "beef", "bread", "home", "life"}; //List of words
@@ -18,8 +18,24 @@ public class MainHangman
             if (guess.equals(wordList[randomWord]))
                 System.out.println("Correct! Your word was: " + guess); //Output user input
             else {
-                System.out.println(" That is not a valid guess. Try again!");
+                System.out.println("That is not a valid guess. Try again!");
             }
         }
+    }*/
+
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+         String word = "hello";
+         char[] letters = word.toCharArray();
+         for(int wordIndex = 0; wordIndex < letters.length; wordIndex++){
+             String guess = input.nextLine(); //read user input
+             if (guess.equals(letters[0]))
+                System.out.println("Correct!");
+             else {
+                 System.out.println("That is not a valid guess. Try again!");
+             }
+         }
     }
 }
+
