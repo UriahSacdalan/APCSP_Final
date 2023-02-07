80969900725b52c15bbdbd7025bab0c6175ae4b9
Uriah <uriahnaomi.sacdalan@vtsd.com>
2023-02-07 11:45:01 -0500

    dnf

--- MainHangman.java
+++ MainHangman.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 /* Needs: selection (if), iteration (loop i.e. for), sequences (put it in order), user-defined methods that call other user-defined methods
 */
-public class MainHangman
-{
+public class MainHangman {
     /*public static void main(String[] args)
     {
         Random testRand = new Random();
@@ -23,27 +22,40 @@ public class MainHangman
         }
     }*/
 
-    public static void main(String[] args)
-    {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Random testRand = new Random();
 
         String wordList[] = {"art", "scone", "beef", "bread", "home", "life"}; //List of words
-        int randomWord = testRand.nextInt(wordList.length); //randomly choose from list
-        char[] letters = wordList[randomWord].toCharArray(); //make letters individual characters(?)
 
-        //int guessLetters = letters.length; Tries? for loop ineffective... maybe?? I DONT KNOWWW
 
-        for(int tries = 6; tries > 0; tries--) {
-            System.out.println("Tries left: " + tries); //Displays # of tries
-            System.out.print("Enter guess: ");
-            String guess = input.nextLine(); //read user input
-            if (guess.equals(wordList[randomWord]))
-            System.out.println("Correct! Your word was: " + guess); //Output user input
-            else {
-                System.out.println("That is not a valid guess. Try again!");
-             }
-         }
+        boolean gameStart = true;
+        while (gameStart) {
+            System.out.println("Guess my word! \n");
+            int randomWord = testRand.nextInt(wordList.length); //randomly choose from list
+            char[] letters = wordList[randomWord].toCharArray(); //make letters individual characters(?)
+            int triesToGuess = letters.length; //# of Tries; for loop ineffective... maybe?? I DONT KNOWWW
+            char blank[] = new char [triesToGuess]; //used as a container for letters
+
+            //loop will assign blanks corresponding to letters
+            for (int blankLoop = 0; blankLoop < blank.length; blankLoop++) {
+                blank[blankLoop] = '_';
+            }
+            boolean winner = false;
+            int tries = 0;
+            while(!winner && tries != triesToGuess)
+                System.out.println("Letters left: " + blank); //Displays # of tries
+
+                System.out.print("Enter one letter: ");
+                char guess = scanner.next().charAt(0); //read user input
+                if (guess == '_'){
+                   System.out.println("Input: " + guess);
+                }
+                /*    System.out.println("Correct! Your word was: " + guess); //Output user input
+                else {
+                    System.out.println("That is not a valid guess. Try again!");
+                }*/
+
+        }
     }
 }
-
