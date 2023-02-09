b3e97c86e69a2901c141c7bd6c9e06efb3150f61
Uriah <uriahnaomi.sacdalan@vtsd.com>
2023-02-09 13:00:24 -0500

    dnf

--- MainHangman.java
+++ MainHangman.java
@@ -3,52 +3,32 @@ import java.util.Random;
 /* Needs: selection (if), iteration (loop i.e. for), sequences (put it in order), user-defined methods that call other user-defined methods
 */
 public class MainHangman {
-    /*public static void main(String[] args)
-    {
-        Random testRand = new Random();
-        String wordList[] = {"art", "scone", "beef", "bread", "home", "life"}; //List of words
-        int randomWord = testRand.nextInt(wordList.length); //randomly choose from list
-
-        Scanner input = new Scanner(System.in);
-        for(int tries = 6; tries > 0; tries--) {
-            System.out.println("Tries left: " + tries); //Displays # of tries
-            System.out.print("Enter guess: ");
-            String guess = input.nextLine(); //read user input
-            if (guess.equals(wordList[randomWord]))
-                System.out.println("Correct! Your word was: " + guess); //Output user input
-            else {
-                System.out.println("That is not a valid guess. Try again!");
-            }
-        }
-    }*/
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random testRand = new Random();
 
         String wordList[] = {"art", "scone", "beef", "bread", "home", "life"}; //List of words
 
-
         boolean gameStart = true;
         while (gameStart) {
             System.out.println("Guess my word! \n");
             int randomWord = testRand.nextInt(wordList.length); //randomly choose from list
-            char[] letters = wordList[randomWord].toCharArray(); //make letters individual characters(?)
-            int triesToGuess = letters.length; //# of Tries; for loop ineffective... maybe?? I DONT KNOWWW
+            char[] letters = wordList[randomWord].toCharArray(); //make letters individual characters
+            int triesToGuess = letters.length; //# of Tries
             char blank[] = new char[triesToGuess]; //used as a container for letters
 
             //loop will assign blanks corresponding to letters
             for (int blankLoop = 0; blankLoop < blank.length; blankLoop++) {
                 blank[blankLoop] = '_';
             }
-            boolean winner = false;
+            /*boolean winner = false;
             int tries = 0;
             while (!winner && tries != triesToGuess) {
                 System.out.print("Letters left: "); //Displays letters in word
                 System.out.println(blank);
 
-                int lives = triesToGuess-tries;
-                System.out.println("You have " + lives + " letters left. \n");
+                int lettersRemain = triesToGuess-tries;
+                System.out.println("You have " + lettersRemain + " letters left. \n");
 
                 System.out.print("Enter one letter: ");
                 char guess = scanner.next().charAt(0); //read user input
@@ -57,14 +37,29 @@ public class MainHangman {
                     gameStart = false;
                 }
                 else{
-                    System.out.println("Input: " + guess);
+                    for(int index = 0; index < letters.length; index++){
+                        if(letters[index] == guess){
+                            blank[index] = guess;
+                        }
+                    }
                 }
-                /*    System.out.println("Correct! Your word was: " + guess); //Output user input
-                else {
-                    System.out.println("That is not a valid guess. Try again!");
-                }*/
+            }*/
+        }
+    }
+    public static void print(char array[]){
+        for(int index = 0; index < array.length; index++){
+            System.out.print(array[index] + " ");
+        }
+        System.out.println();
+    }
 
+    public static boolean didYouWin(char array[]){
+        boolean win = true;
+        for(int index = 0; index < array.length; index++){
+            if(array[index] == '_'){
+                win = false;
             }
         }
+        return win;
     }
 }
