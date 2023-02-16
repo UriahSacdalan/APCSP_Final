34745b7aae5ca1123376f3306476cba9e9c4c9e4
Uriah <uriahnaomi.sacdalan@vtsd.com>
2023-02-16 12:08:41 -0500

    almost finished

--- MainHangman.java
+++ MainHangman.java
@@ -1,19 +1,47 @@
+import com.sun.tools.javac.Main;
+
 import java.util.Scanner;
 import java.util.Random;
 /* Needs: selection (if), iteration (loop i.e. for), sequences (put it in order), user-defined methods that call other user-defined methods
 */
 public class MainHangman {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static Scanner scanner = new Scanner(System.in);
+    public static boolean gameStart;
+    public static boolean winner;
+
+    public static void setTrue(){
+        gameStart = true;
+    }
+    public static void setFalse(){
+        winner = false;
+    }
+
+    public static void playAgain(){
+        if(!winner){
+            System.out.println("Sorry, you're out of tries!");
+        }
+        System.out.println("Want to try again? (Yes/No)");
+        String playAgain = scanner.nextLine();
+        if (playAgain.equals("No")){
+            gameStart = false;
+        }
+        else if (playAgain.equals("Yes")){
+            gameStart = true;
+        }
+    }
+
+    public static void main(String[] args){
+        MainHangman hangman = new MainHangman();
+
         Random random = new Random();
 
         String wordList[] = {"art", "scone", "beef", "bread", "home", "life"}; //List of words
 
-
-        boolean gameStart = true;
+        hangman.setTrue();
         while (gameStart) {
             System.out.println("Guess my word! \n");
             int randomWord = random.nextInt(wordList.length); //randomly choose from list
+            String word = wordList[randomWord];
             char[] letters = wordList[randomWord].toCharArray(); //make letters individual characters(?)
             int triesToGuess = letters.length; //# of Tries; for loop ineffective... maybe?? I DONT KNOWWW
             char blank[] = new char[triesToGuess]; //used as a container for letters
@@ -22,12 +50,14 @@ public class MainHangman {
             for (int blankLoop = 0; blankLoop < blank.length; blankLoop++) {
                 blank[blankLoop] = '_';
             }
-            boolean winner = false;
+
+            hangman.setFalse();
             int tries = 0;
             while(!winner && tries != triesToGuess){
                 System.out.println("Letters left: "); //Displays letters in word
                 print(blank);
-                System.out.printf("You have %d tries left. \n", triesToGuess-tries + 1);
+                int lives = triesToGuess-tries;
+                System.out.printf("You have %d tries left. \n", lives);
                 System.out.println("Enter one letter: ");
 
                 char guess = scanner.nextLine().charAt(0); //read user input
@@ -37,26 +67,19 @@ public class MainHangman {
                     winner = true;
                     gameStart = false;
                 } else{
-                    for(int i=0; i<letters.length; i++){
-                        if(letters[i] == guess){
-                            blank[i] = guess;
+                    for(int index = 0; index < letters.length; index++){
+                        if(letters[index] == guess){
+                            blank[index] = guess;
                         }
                     }
 
                     if(didYouWin(blank)){
                         winner = true;
-                        System.out.println("You got it!");
+                        System.out.println("You got it! Your word was " + word + "!");
                     }
                 }
             }
-            if(!winner){
-                System.out.println("Sorry, you're out of tries!");
-            }
-            System.out.println("Want to try again? (Yes/No)");
-            String playAgain = scanner.nextLine();
-            if (playAgain.equals("No")){
-                gameStart = false;
-            }
+            hangman.playAgain();
         }
         System.out.println("Game Over!");
     }
