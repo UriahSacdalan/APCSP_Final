f6fc8aa542e316e262644242a12c4cb3f9c6d7e0
Uriah <uriahnaomi.sacdalan@vtsd.com>
2023-02-17 11:43:33 -0500

    FINISHED!! Kind of... do written first

--- MainHangman.java
+++ MainHangman.java
@@ -6,8 +6,16 @@ import java.util.Random;
 */
 public class MainHangman {
     public static Scanner scanner = new Scanner(System.in);
+    public static Random random = new Random();
     public static boolean gameStart;
+    public static String wordList[] = {"art", "scone", "beef", "bread", "head", "life"}; //List of words
+    public static int randomWord = random.nextInt(wordList.length); //randomly choose from list
+    public static String word = wordList[randomWord];
+    public static char[] letters = wordList[randomWord].toCharArray();
+    public static int triesToGuess = letters.length;
+    public static char blank[] = new char[triesToGuess];
     public static boolean winner;
+
 
     public static void setTrue(){
         gameStart = true;
@@ -19,18 +27,9 @@ public class MainHangman {
     public static void main(String[] args){
         MainHangman hangman = new MainHangman();
 
-        Random random = new Random();
-
-        String wordList[] = {"art", "scone", "beef", "bread", "head", "life"}; //List of words
-
         hangman.setTrue();
         while (gameStart) {
             System.out.println("Guess my word! \n");
-            int randomWord = random.nextInt(wordList.length); //randomly choose from list
-            String word = wordList[randomWord];
-            char[] letters = wordList[randomWord].toCharArray(); //make letters individual characters(?)
-            int triesToGuess = letters.length; //# of Tries; for loop ineffective... maybe?? I DONT KNOWWW
-            char blank[] = new char[triesToGuess]; //used as a container for letters
 
             //loop will assign blanks corresponding to letters
             for (int blankLoop = 0; blankLoop < blank.length; blankLoop++) {
@@ -45,35 +44,39 @@ public class MainHangman {
                 int lives = triesToGuess-tries;
                 System.out.printf("You have %d tries left. \n", lives);
                 System.out.println("Enter one letter: ");
-
-                char guess = scanner.nextLine().charAt(0); //read user input
                 tries++;
 
-                if(guess == '-'){
-                    winner = true;
-                    gameStart = false;
-                } else{
-                    for(int index = 0; index < letters.length; index++){
-                        if(letters[index] == guess){
-                            blank[index] = guess;
-                        }
-                    }
-
-                    if(didYouWin(blank)){
-                        winner = true;
-                        System.out.println("You got it! Your word was " + word + "!");
-                    }
-                }
+                hangman.readInput();
             }
             hangman.playAgain();
         }
         System.out.println("Game Over!");
     }
+
     public static void print(char array[]){
         for(int index = 0; index < array.length; index++){
             System.out.print(array[index] + " ");
         }
         System.out.println();
+    }
+
+    public static void readInput(){
+        char guess = scanner.nextLine().charAt(0); //read user input
+        if(guess == '-'){
+            winner = true;
+            gameStart = false;
+        } else{
+            for(int index = 0; index < letters.length; index++){
+                if(letters[index] == guess){
+                    blank[index] = guess;
+                }
+            }
+
+            if(didYouWin(blank)){
+                winner = true;
+                System.out.println("You got it! Your word was " + word + "!");
+            }
+        }
     }
 
     public static boolean didYouWin(char array[]){
@@ -85,6 +88,7 @@ public class MainHangman {
         }
         return win;
     }
+
     public static void playAgain(){
         if(!winner){
             System.out.println("Sorry, you're out of tries!");
@@ -101,9 +105,7 @@ public class MainHangman {
             gameStart = true;
         }
         else if (playAgain.equals("yes")){
-
             gameStart = true;
         }
     }
-
 }
