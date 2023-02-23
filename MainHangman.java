bcbe204973504f75a2dfd691fb14ae34dd6bef37
Uriah <uriahnaomi.sacdalan@vtsd.com>
2023-02-23 12:29:15 -0500

    FINISHED!! Kind of... do written first

--- MainHangman.java
+++ MainHangman.java
@@ -17,14 +17,68 @@ public class MainHangman {
     public static boolean winner;
 
 
-    public static void setTrue(){
+    public static void setTrue() {
         gameStart = true;
     }
-    public static void setFalse(){
+
+    public static void setFalse() {
         winner = false;
     }
 
-    public static void main(String[] args){
+    public static void print(char array[]) {
+        for (int index = 0; index < array.length; index++) {
+            System.out.print(array[index] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void readInput() {
+        char guess = scanner.nextLine().charAt(0); //read user input
+        if (guess == '-') {
+            winner = true;
+            gameStart = false;
+        } else {
+            for (int index = 0; index < letters.length; index++) {
+                if (letters[index] == guess) {
+                    blank[index] = guess;
+                }
+            }
+
+            if (didYouWin(blank)) {
+                winner = true;
+                System.out.println("You got it! Your word was " + word + "!");
+            }
+        }
+    }
+
+    public static boolean didYouWin(char array[]) {
+        boolean win = true;
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == '_') {
+                win = false;
+            }
+        }
+        return win;
+    }
+
+    public static void playAgain() {
+        if (!winner) {
+            System.out.println("Sorry, you're out of tries!");
+        }
+        System.out.println("Want to try again? (Yes/No)");
+        String playAgain = scanner.nextLine();
+        if (playAgain.equals("No")) {
+            gameStart = false;
+        } else if (playAgain.equals("no")) {
+            gameStart = false;
+        } else if (playAgain.equals("Yes")) {
+            gameStart = true;
+        } else if (playAgain.equals("yes")) {
+            gameStart = true;
+        }
+    }
+
+    public static void main(String[] args) {
         MainHangman hangman = new MainHangman();
 
         hangman.setTrue();
@@ -38,10 +92,10 @@ public class MainHangman {
 
             hangman.setFalse();
             int tries = 0;
-            while(!winner && tries != triesToGuess){
+            while (!winner && tries != triesToGuess) {
                 System.out.println("Letters left: "); //Displays letters in word
                 print(blank);
-                int lives = triesToGuess-tries;
+                int lives = triesToGuess - tries;
                 System.out.printf("You have %d tries left. \n", lives);
                 System.out.println("Enter one letter: ");
                 tries++;
@@ -51,61 +105,5 @@ public class MainHangman {
             hangman.playAgain();
         }
         System.out.println("Game Over!");
-    }
-
-    public static void print(char array[]){
-        for(int index = 0; index < array.length; index++){
-            System.out.print(array[index] + " ");
-        }
-        System.out.println();
-    }
-
-    public static void readInput(){
-        char guess = scanner.nextLine().charAt(0); //read user input
-        if(guess == '-'){
-            winner = true;
-            gameStart = false;
-        } else{
-            for(int index = 0; index < letters.length; index++){
-                if(letters[index] == guess){
-                    blank[index] = guess;
-                }
-            }
-
-            if(didYouWin(blank)){
-                winner = true;
-                System.out.println("You got it! Your word was " + word + "!");
-            }
-        }
-    }
-
-    public static boolean didYouWin(char array[]){
-        boolean win = true;
-        for(int index = 0; index < array.length; index++){
-            if(array[index] == '_'){
-                win = false;
-            }
-        }
-        return win;
-    }
-
-    public static void playAgain(){
-        if(!winner){
-            System.out.println("Sorry, you're out of tries!");
-        }
-        System.out.println("Want to try again? (Yes/No)");
-        String playAgain = scanner.nextLine();
-        if (playAgain.equals("No")){
-            gameStart = false;
-        }
-        else if (playAgain.equals("no")){
-            gameStart = false;
-        }
-        else if (playAgain.equals("Yes")){
-            gameStart = true;
-        }
-        else if (playAgain.equals("yes")){
-            gameStart = true;
-        }
     }
 }
